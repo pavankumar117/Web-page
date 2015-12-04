4505ba7d2adf04784f19bf97cc63b8942c02fff2
PavanKumar <pavankumar117@yahoo.com>
2015-12-04 09:09:58 +0530

    Web page modified

--- web.js
+++ web.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function(){	
+  $("#circle1").css("border", "2px solid black");
+  $('#quotedetails').show();
+  $('#additionalinfo').hide();
+  $('#noteto').hide();
   $('#circle1').click(function(){
     $("#circle1").css("border", "2px solid black");
     $("#circle2").css("border", "0px solid black");
@@ -23,13 +27,45 @@ $(document).ready(function(){
     $('#additionalinfo').hide();
     $('#noteto').show();
   });
-});
+  $('#next1').click(function() {
+	var y = $('#quotename');
+	console.log(y);
+	if(y === null ){
+		alert('fill all boxes');
+	}
+	$("#circle1").css("border", "0px solid black");
+	$("#circle2").css("border", "2px solid black");
+	$("#circle3").css("border", "0px solid black");	
+	$('#quotedetails').hide();
+	$('#additionalinfo').show();
+	$('#noteto').hide();
+	
+		//alert('hello');
+	
+  });
+  $('#next2').click(function(){
+	$("#circle1").css("border", "0px solid black");
+	$("#circle2").css("border", "0px solid black");
+	$("#circle3").css("border", "2px solid black");
+	$('#quotedetails').hide();
+	$('#additionalinfo').hide();
+	$('#noteto').show();
+  });
+  $('#back2').click(function(){
+	$("#circle1").css("border", "2px solid black");
+	$("#circle2").css("border", "0px solid black");
+	$("#circle3").css("border", "0px solid black");
+	$('#quotedetails').show();
+	$('#additionalinfo').hide();
+	$('#noteto').hide();
+  });
+  $('#back3').click(function(){
+	$("#circle1").css("border", "0px solid black");
+	$("#circle2").css("border", "2px solid black");
+	$("#circle3").css("border", "0px solid black");
+	$('#quotedetails').hide();
+	$('#additionalinfo').show();
+	$('#noteto').hide();
+  });
 
-#('#next1').click(function(){
-  $("#circle1").css("border", "0px solid black");
-  $("#circle2").css("border", "2px solid black");
-  $("#circle3").css("border", "0px solid black");
-  $('#quotedetails').hide();
-  $('#additionalinfo').show();
-  $('#noteto').hide();
 });
